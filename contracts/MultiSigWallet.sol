7fa629c5077cf9ecb661fcdedc6f5ad3adaf137b
Stefan George <Georgi87@users.noreply.github.com>
2016-09-10 09:06:38 +0200

    added pragma solidity ^0.4.0;

--- contracts/MultiSigWallet.sol
+++ contracts/MultiSigWallet.sol
@@ -1,3 +1,5 @@
+pragma solidity ^0.4.0;
+
 /// @title Multisignature wallet - Allows multiple parties to agree on transactions before execution.
 /// @author Stefan George - <stefan.george@consensys.net>
 contract MultiSigWallet {

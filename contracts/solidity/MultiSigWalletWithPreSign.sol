44499355b2b1bfbace09150ac51c5c7c1af27f67
Stefan George <noreply@sgeorge.de>
2016-11-16 12:46:44 +0100

    added doc comments to pre sign wallet

--- contracts/solidity/MultiSigWalletWithPreSign.sol
+++ contracts/solidity/MultiSigWalletWithPreSign.sol
@@ -13,6 +13,9 @@ contract MultiSigWalletWithPreSign is MultiSigWallet {
         _;
     }
 
+    /// @dev Contract constructor sets initial owners and required number of confirmations.
+    /// @param _owners List of initial owners.
+    /// @param _required Number of required confirmations.
     function MultiSigWalletWithPreSign(address[] _owners, uint _required)
         public
         MultiSigWallet(_owners, _required)
@@ -20,6 +23,14 @@ contract MultiSigWalletWithPreSign is MultiSigWallet {
         // Nothing to do here
     }
 
+    /// @dev Allows to submit a transaction with multiple owner signatures.
+    /// @param destination Transaction target address.
+    /// @param value Transaction ether value.
+    /// @param data Transaction data payload.
+    /// @param nonce Internal transaction nonce to identify transactions with identical arguments.
+    /// @param v List of v parameters of owner signatures.
+    /// @param rs List of r and s parameters of owner signatures.
+    /// @return transactionHash Returns hash identifying a transaction.
     function submitTransactionPreSigned(address destination, uint value, bytes data, uint nonce, uint8[] v, bytes32[] rs)
         public
         returns (bytes32 transactionHash)
@@ -28,6 +39,10 @@ contract MultiSigWalletWithPreSign is MultiSigWallet {
         confirmTransactionPreSigned(transactionHash, v, rs);
     }
 
+    /// @dev Allows to confirm a transaction with multiple owner signatures.
+    /// @param transactionHash Hash identifying a transaction.
+    /// @param v List of v parameters of owner signatures.
+    /// @param rs List of r and s parameters of owner signatures.
     function confirmTransactionPreSigned(bytes32 transactionHash, uint8[] v, bytes32[] rs)
         public
         signaturesFromOwners(transactionHash, v, rs)
@@ -37,6 +52,12 @@ contract MultiSigWalletWithPreSign is MultiSigWallet {
         executeTransaction(transactionHash);
     }
 
+    /// @dev Allows to calculate the transaction hash for given transaction arguments.
+    /// @param destination Transaction target address.
+    /// @param value Transaction ether value.
+    /// @param data Transaction data payload.
+    /// @param nonce Internal transaction nonce to identify transactions with identical arguments.
+    /// @return transactionHash Returns hash identifying a transaction.
     function calcTransactionHash(address destination, uint value, bytes data, uint nonce)
         public
         constant

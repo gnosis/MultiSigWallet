fb5f3d3b9e6608c1aa7662a5fbdb7c001bb28150
Denís Graña <denisgranha@gmail.com>
2016-11-27 02:14:08 +0100

    send transaction view

--- dapp/controllers/sendTransactionCtrl.js
+++ dapp/controllers/sendTransactionCtrl.js
@@ -2,8 +2,37 @@
   function(){
     angular
     .module("multiSigWeb")
-    .controller("sendTransactionCtrl.js", function($scope, Wallet, Utils){
+    .controller("sendTransactionCtrl", function($scope, Wallet, Utils){
+      $scope.methods = [];
+      $scope.tx = {};
 
+      $scope.$watch(
+        function(){
+          return Wallet.txParams.nonce;
+        },
+        function(){
+          $scope.tx.nonce = Wallet.txParams.nonce;
+        }
+      );
+
+      $scope.send = function(){
+        // if method, use contract instance method TODO
+        if($scope.method){
+
+        }
+        else{
+          
+        }
+      }
+
+      $scope.updateMethods = function(){
+        $scope.abiArray = JSON.parse($scope.abi);
+        $scope.abiArray.map(function(item, index){
+          if(!item.constant && item.name){
+            $scope.methods.push({name: item.name, index: index});
+          }
+        });
+      }
     });
   }
 )();

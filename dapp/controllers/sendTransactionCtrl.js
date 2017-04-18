2b31a74af9d59f2d75086b721451aadabd0bc73f
Denís Graña <denisgranha@gmail.com>
2017-04-18 23:44:50 +0200

    allow simulate calls without parameters

--- dapp/controllers/sendTransactionCtrl.js
+++ dapp/controllers/sendTransactionCtrl.js
@@ -17,7 +17,7 @@
         if ($scope.method) {
           params.map(function(param, index) {
             // Parse if array param
-            if ($scope.method.inputs && $scope.method.inputs[index].type.indexOf("[]") !== -1) {
+            if ($scope.method.inputs && $scope.method.inputs.length && $scope.method.inputs[index].type.indexOf("[]") !== -1) {
               try {
                 params[index] = JSON.parse($scope.params[index]);
               }
@@ -83,7 +83,7 @@
         if ($scope.method) {
           params.map(function(param, index) {
             // Parse if array param
-            if ($scope.method.inputs && $scope.method.inputs[index].type.indexOf("[]") !== -1) {
+            if ($scope.method.inputs && $scope.method.inputs.length && $scope.method.inputs[index].type.indexOf("[]") !== -1) {
               try {
                 params[index] = JSON.parse($scope.params[index]);
               }

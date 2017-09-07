a3fc6e42bbe841eac018eaf1d9da4684e16b36db
Denis Granha <denisgranha@gmail.com>
2017-09-07 16:57:37 +0200

    use phantomJS instead of Chrome

--- dapp/config.karma.js
+++ dapp/config.karma.js
@@ -71,7 +71,7 @@ module.exports = function(config) {
 
     // start these browsers
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    browsers: ['Chrome'], //['PhantomJS'], //['Chrome'],
+    browsers: ['PhantomJS'], //['PhantomJS'], //['Chrome'],
 
 
     // Continuous Integration mode

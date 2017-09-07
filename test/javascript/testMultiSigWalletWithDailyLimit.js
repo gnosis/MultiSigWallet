01dbaea2d706b3685d73a95861d0125b83b33ff0
Denis Granha <denisgranha@gmail.com>
2017-09-07 12:39:07 +0200

    updated multisig with daily limit test

--- test/javascript/testMultiSigWalletWithDailyLimit.js
+++ test/javascript/testMultiSigWalletWithDailyLimit.js
@@ -4,19 +4,83 @@ const deployMultisig = (owners, confirmations, limit) => {
     return MultiSigWalletWithDailyLimit.new(owners, confirmations, limit)
 }
 
+const utils = require('./utils')
+
 contract('MultiSigWalletWithDailyLimit', (accounts) => {
     let multisigInstance
+    const dailyLimit = 3000
+    const requiredConfirmations = 2
 
     beforeEach(async () => {
-        multisigInstance = await deployMultisig([accounts[0], accounts[1]], 2, 0)
+        multisigInstance = await deployMultisig([accounts[0], accounts[1]], requiredConfirmations, dailyLimit)
         assert.ok(multisigInstance)
     })
 
     it('create multisig', async () => {
         const deposit = 10000
         
+        // Send money to wallet contract
         await new Promise((resolve, reject) => web3.eth.sendTransaction({to: multisigInstance.address, value: deposit, from: accounts[0]}, e => (e ? reject(e) : resolve())))
-        const balance = await new Promise((resolve, reject) => web3.eth.getBalance(multisigInstance.address, (e, balance) => (e ? reject(e) : resolve(balance))))
+        const balance = await utils.balanceOf(web3, multisigInstance.address)
         assert.equal(balance.valueOf(), deposit)
+        assert.equal(dailyLimit, await multisigInstance.dailyLimit())
+        assert.equal(dailyLimit, await multisigInstance.calcMaxWithdraw())
+
+        // Withdraw daily limit
+        const value1 = 2000
+        let owner1Balance = await utils.balanceOf(web3, accounts[0])
+        await multisigInstance.submitTransaction(accounts[0], value1, "", {from: accounts[1]})
+        assert.equal(
+            owner1Balance.add(value1).toString(),
+            (await utils.balanceOf(web3, accounts[0])).toString()
+        )
+        assert.equal(
+            balance.sub(value1).toString(),
+            (await utils.balanceOf(web3, multisigInstance.address)).toString()
+        )
+
+        // Update daily limit
+        const dailyLimitUpdated = 2000
+        const dailyLimitEncoded = multisigInstance.contract.changeDailyLimit.getData(dailyLimitUpdated)
+        const transactionId = utils.getParamFromTxEvent(
+            await multisigInstance.submitTransaction(multisigInstance.address, 0, dailyLimitEncoded, {from: accounts[0]}),
+            'transactionId', null, 'Submission')
+        
+        await multisigInstance.confirmTransaction(transactionId, {from: accounts[1]})
+        assert.equal(dailyLimitUpdated, await multisigInstance.dailyLimit())
+        assert.equal(0, await multisigInstance.calcMaxWithdraw())
+
+        await utils.increaseTimestamp(web3, 24*3600+1)
+        assert.equal(dailyLimitUpdated, (await multisigInstance.calcMaxWithdraw()).toNumber())
+
+        // Withdraw daily limit
+        const value2 = 1000
+        owner1Balance = await utils.balanceOf(web3, accounts[0])
+        await multisigInstance.submitTransaction(accounts[0], value2, "", {from: accounts[1]})
+        assert.equal(
+            owner1Balance.add(value2).toString(),
+            (await utils.balanceOf(web3, accounts[0])).toString()
+        )
+        assert.equal(
+            deposit-value2-value1,
+            await utils.balanceOf(web3, multisigInstance.address)
+        )
+        assert.equal(
+            dailyLimitUpdated - value2,
+            await multisigInstance.calcMaxWithdraw()
+        )
+        await multisigInstance.submitTransaction(accounts[0], value2, "", {from: accounts[1]})
+        assert.equal(
+            owner1Balance.add(value2*2).toString(),
+            (await utils.balanceOf(web3, accounts[0])).toString()
+        )
+        assert.equal(
+            deposit-value2*2-value1,
+            await utils.balanceOf(web3, multisigInstance.address)
+        )
+        assert.equal(
+            dailyLimitUpdated - value2*2,
+            await multisigInstance.calcMaxWithdraw()
+        )
     })
 })
---
+++ test/javascript/utils.js
@@ -0,0 +1,45 @@
+function getParamFromTxEvent(transaction, paramName, contractFactory, eventName) {
+    assert.isObject(transaction)
+    let logs = transaction.logs
+    if(eventName != null) {
+        logs = logs.filter((l) => l.event === eventName)
+    }
+    assert.equal(logs.length, 1, 'too many logs found!')
+    let param = logs[0].args[paramName]
+    if(contractFactory != null) {
+        let contract = contractFactory.at(param)
+        assert.isObject(contract, `getting ${paramName} failed for ${param}`)
+        return contract
+    } else {
+        return param
+    }
+}
+
+function mineBlock(web3, reject, resolve) {
+    web3.currentProvider.sendAsync({
+        method: "evm_mine",
+        jsonrpc: "2.0",
+        id: new Date().getTime()
+      }, (e) => (e ? reject(e) : resolve()))
+}
+
+function increaseTimestamp(web3, increase) {
+    return new Promise((resolve, reject) => {
+        web3.currentProvider.sendAsync({
+            method: "evm_increaseTime",
+            params: [increase],
+            jsonrpc: "2.0",
+            id: new Date().getTime()
+          }, (e) => (e ? reject(e) : mineBlock(web3, reject, resolve)))
+    })    
+}
+
+function balanceOf(web3, account) {
+    return new Promise((resolve, reject) => web3.eth.getBalance(account, (e, balance) => (e ? reject(e) : resolve(balance))))
+}
+
+Object.assign(exports, {
+    getParamFromTxEvent,
+    increaseTimestamp,
+    balanceOf,
+})

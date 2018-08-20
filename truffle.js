3f7e323db8c5d238703b79913ebaac10d89600a5
Rodrigo Acosta <rodrigo.acosta@smartvalor.com>
2018-08-19 21:41:57 -0300

    added ropsten configuration

--- truffle.js
+++ truffle.js
@@ -1,3 +1,5 @@
+
+const HDWalletProvider = require("truffle-hdwallet-provider");
 module.exports = {
   networks: {
     development: {
@@ -6,6 +8,12 @@ module.exports = {
       network_id: "*", // Match any network id
       gas: 4000000,
       gasPrice: 10000000000, // 10 gwei
+    },
+    ropsten:  {
+      provider: () => new HDWalletProvider("tube column poverty remind rebuild skate dad attack hurry waste twenty amount", "https://ropsten.infura.io/mUibnipr7Lc5imbkKKDT"),
+      network_id: 3,
+      gas: 3000000,
+      gasPrice: 21
     }
   }
 };

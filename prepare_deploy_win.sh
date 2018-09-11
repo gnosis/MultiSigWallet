7dd0f599f7de361e6bde08d3018f1de7b949d84e
Giacomo Licari <giacomo.licari@gmail.com>
2018-09-11 13:21:11 +0200

    Remove electron-rebuild

--- prepare_deploy_win.sh
+++ prepare_deploy_win.sh
@@ -2,12 +2,11 @@
 
 echo "=== PREPARE DEPLOY ==="
 
-echo "=== Appveyor === %APPVEYOR%" 
-
 if [[ ${APPVEYOR_REPO_TAG} == true ]]; then
   # it is a TAG
   # rename .exe file
-  mv multisigweb*.exe multisigweb-$APPVEYOR_REPO_TAG.exe
+  echo "Renaming multisigweb.*.exe to multisigweb-$APPVEYOR_REPO_TAG.exe"
+  mv /dapp/dist/multisigweb*.exe /dapp/dist/multisigweb-$APPVEYOR_REPO_TAG.exe
 fi
 
 echo "=== PREPARE DEPLOY DONE ==="

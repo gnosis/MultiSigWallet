09aba1670210cf1774ef159d1981708dd058794d
Denis Rojs <denis.rojs@nicehash.com>
2020-11-02 20:09:35 +0100

    compiler 0.4.15

--- truffle.js
+++ truffle.js
@@ -1,4 +1,11 @@
 module.exports = {
+  compilers: {
+    solc: {
+        version: "0.4.15", // A version or constraint - Ex. "^0.5.0"
+                         // Can also be set to "native" to use a native solc
+        parser: "solcjs"  // Leverages solc-js purely for speedy parsing
+    }
+  },
   networks: {
     development: {
       host: "localhost",
